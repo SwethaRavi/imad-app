d5dc46ffebfb942248fcf8e7d80fe355174c5607
SwethaRavi <swetha95.ravichandran@gmail.com>
2017-08-16 15:35:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function(req,res){
+   counter = counter + 1;
+   res.send(counter.toString());
+});
 app.get('/article-one', function(req,res){
    res.send('Article one is requested and is served here');   
 });
