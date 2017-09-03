34a675f5e09f30d2b7f393168ececcd0ac66b08f
SwethaRavi <swetha95.ravichandran@gmail.com>
2017-09-03 16:37:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,21 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-
-var counter = 0;
-app.get('/counter', function(req,res){
-   counter = counter + 1;
-   res.send(counter.toString());
-});
-app.get('/article-one', function(req,res){
-   res.send('Article one is requested and is served here');   
-});
-app.get('/article-two', function(req,res){
-   res.send('Article two is requested and is served here');   
-});
-app.get('/article-three', function(req,res){
-   res.send('Article three is requested and is served here');   
-});
+var app = express();
+app.use(morgan('combined'));
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -26,9 +13,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
