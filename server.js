0312f1cadb7aa692c315b86c01cebdc3b67da0f3
SwethaRavi <swetha95.ravichandran@gmail.com>
2017-09-03 16:33:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,33 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
 
-var config = {
-   user : 'swetha95ravichandran',
-   database : 'swetha95ravichandran',
-   host : 'db.imad.hasura-app.io',
-   port : '5432',
-   password : process.env.DB_PASSWORD
-}
-var app = express();
-app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var pool = new Pool(config);
-app.get('/test-db', function(req, res){
-    pool.query('SELECT * FROM test', function(err, result){
-        if(err){
-        res.status(500).send(err.toString());
-    }else{
-        res.send(JSON.stringify(result));
-    }
-    
-    });
-});
 
 var counter = 0;
 app.get('/counter', function(req,res){
@@ -42,6 +16,10 @@ app.get('/article-two', function(req,res){
 });
 app.get('/article-three', function(req,res){
    res.send('Article three is requested and is served here');   
+});
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
